e6505b0179cf2e9c63f412dafd890611a1b1ee12
sunxinxin <1643066653@qq.com>
2020-02-12 23:15:02 +0800

    init repo

---
+++ src/main/java/com/sunxinxin/yule/YuleApplication.java
@@ -0,0 +1,13 @@
+package com.sunxinxin.yule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sunxinxin/yule/YuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunxinxin.yule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
